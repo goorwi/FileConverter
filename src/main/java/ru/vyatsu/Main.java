31f6feb14dc916c6a2a96778d94281abd571a044
goorwi <russkih.vlad123@gmail.com>
2023-11-03 12:17:39 +0300

    работа с интерфейсом

--- src/main/java/ru/vyatsu/Main.java
+++ src/main/java/ru/vyatsu/Main.java
@@ -1,7 +1,39 @@
 package ru.vyatsu;
 
-public class Main {
-    public static void main(String[] args) {
+import ru.vyatsu.service.JsonToXmlConverter;
+import ru.vyatsu.service.XmlToJsonConverter;
 
+import javax.xml.bind.JAXBException;
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) throws JAXBException, FileNotFoundException {
+        if (args.length == 2)
+        {
+            String input = args[0];
+            String output = args[1];
+            if (input.endsWith(".xml") && output.endsWith(".json"))
+            {
+                XmlToJsonConverter conv = new XmlToJsonConverter();
+                conv.Convert(input, output);
+
+                System.out.println("Преобразование выполнено успешно!");
+            }
+            else if (input.endsWith(".json") && output.endsWith(".xml"))
+            {
+                JsonToXmlConverter conv = new JsonToXmlConverter();
+                conv.Convert(input, output);
+
+                System.out.println("Преобразование выполнено успешно!");
+            }
+            else {
+                System.out.println("Конвертер предусматривает преобразование из xml в json, либо из json в xml." +
+                        "Иные расширения недоступны");
+            }
+
+        }
+        else {
+            System.out.println("Ошибка! Команда должна содержать 2 аргумента: <исходный файл> <конвертированный файл>");
+        }
     }
 }
